193cc788e13da86ef1ed28543c6bf4bbb033157e
Solal Ohana <solalohana@gmail.com>
2023-06-09 10:19:33 +0300

    changes to the datafiles

--- fileConcat.py
+++ fileConcat.py
@@ -4,20 +4,15 @@ import csv
 # Define the root directory path
 root_dir = "CSV"
 
-# Create a new directory to store the concatenated files
-concatenated_dir = "Concatenated"
-os.makedirs(concatenated_dir, exist_ok=True)
+# Create a new directory to store the centralized CSV file
+os.makedirs("Concatenated", exist_ok=True)
+
+# Create a list to store the rows from all CSV files
+all_rows = []
 
 # Iterate through the directories inside the root directory
 for dir_name in os.listdir(root_dir):
     dir_path = os.path.join(root_dir, dir_name)
-
-    # Create a new directory to store the concatenated file
-    concat_dir = os.path.join(concatenated_dir, dir_name)
-    os.makedirs(concat_dir, exist_ok=True)
-
-    # Create a list to store the rows from all CSV files
-    all_rows = []
 
     # Iterate through the subdirectories inside each directory
     for sub_dir_name in os.listdir(dir_path):
@@ -40,8 +35,16 @@ for dir_name in os.listdir(root_dir):
                 rows = rows[1:]
 
                 # Modify the rows to add the desired columns
-                new_rows = []
+                new_row = []
+
                 for i, row in enumerate(rows):
+                    # Iterate through the values in the row
+                    for j, value in enumerate(row):
+                        # Check if the value is in exponential format
+                        if str.isdigit(value[-1]) and 'E' in value:
+                            # Convert the exponential number to a float
+                            row[j] = float(value)
+
                     # Extract the type and iteration information
                     csv_type = dir_name
                     iteration = "iteration " + csv_file_name[-5]  # Add "iteration" before the number
@@ -52,15 +55,14 @@ for dir_name in os.listdir(root_dir):
                     # Get the directory name of the CSV file
                     file_dir = os.path.basename(os.path.dirname(bee_dir_path))
 
-                    # Create a new row with the added columns
-                    new_row = row + [csv_type, iteration, hive, file_dir]
-                    new_rows.append(new_row)
+                    # Append the values from the current row to the new_row list
+                    new_row.extend(row + [csv_type, iteration, hive, file_dir])
 
-                # Add the modified rows to the list of all rows
-                all_rows.extend(new_rows)
+                # Add the modified row to the list of all rows
+                all_rows.append(new_row)
 
-    # Write the concatenated rows to a new CSV file
-    concat_file_path = os.path.join(concat_dir, "concatenated1.1.csv")
-    with open(concat_file_path, 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(all_rows)
+# Write the concatenated rows to the centralized CSV file
+concat_file_path = os.path.join("Concatenated", "concatenated1.1.csv")
+with open(concat_file_path, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(all_rows)
