ad6a11ad3e49afccc3e97430afa8fa7f626ca0fe
TomerRaitsis <tomerrs81096@gmail.com>
2023-07-10 21:02:32 +0300

    testing 1-2 and 7-8

--- main.py
+++ main.py
@@ -68,13 +68,10 @@ with open(concat_file_path, 'r') as file:
     reader = csv.reader(file)
     next(reader)  # Skip the header row
     for row in reader:
-
         if row[1] != 'iteration 3' and row[1] != 'iteration 4' and row[1] != 'iteration 5' and row[1] != 'iteration 6':
-            print(row[1])
             data.append(row[4])  # Modify the index based on the column position of the data in your CSV
             labels.append(row[1])  # Modify the index based on the column position of the label in your CSV
-#print(data)
-#print(labels)
+
 # Initialize the label encoder
 label_encoder = LabelEncoder()
 labels_encoded = label_encoder.fit_transform(labels)
