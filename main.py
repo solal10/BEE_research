85aa47a5d2da536a9bf54cac3b9f9d37ebcf3496
TomerRaitsis <tomerrs81096@gmail.com>
2023-07-10 21:00:42 +0300

    testing 1-2 and 7-8

--- main.py
+++ main.py
@@ -68,8 +68,9 @@ with open(concat_file_path, 'r') as file:
     reader = csv.reader(file)
     next(reader)  # Skip the header row
     for row in reader:
-        print(row[1])
+
         if row[1] != 'iteration 3' and row[1] != 'iteration 4' and row[1] != 'iteration 5' and row[1] != 'iteration 6':
+            print(row[1])
             data.append(row[4])  # Modify the index based on the column position of the data in your CSV
             labels.append(row[1])  # Modify the index based on the column position of the label in your CSV
 #print(data)
